26ee18c527ea55455193d9c1f29a279b82d2c659
Nevio Pongiluppi <Nevio-source@users.noreply.github.com>
2026-02-24 08:37:11 +0100

    a

---
+++ DB_demo_joins_MariaDB.sql
@@ -0,0 +1,9 @@
+/* ================================================
+   Demo: JOINs in MariaDB
+   Inhalt: Datenbank demo_joins
+	        2 Tabellen: kunden, bestellungen
+           wenig Datensätze für die beiden Tabellen
+           keine CONSTRAINTS
+   ================================================ */
+ 
+/* 0) Datenbank erstellen */
